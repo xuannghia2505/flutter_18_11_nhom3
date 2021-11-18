c2ce2b2f1a1f28bf2f3052c9cd00aff7df7458a3
NghiaBaka <xuannghia255@gmail.com>
2021-11-18 19:26:55 +0700

    Ha Xuan Nghia

---
+++ main.dart
@@ -0,0 +1,23 @@
+
+import 'package:bt_flutter/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
